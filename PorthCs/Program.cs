aa8d712d547df0d0c4e28d49bf54b705409b90e0
Kyle Coffey <me@phytolizer.dev>
2022-01-25 20:19:14 -0600

    Add -r flag to the 'com' subcommand.

--- PorthCs/Program.cs
+++ PorthCs/Program.cs
@@ -8,9 +8,9 @@ internal static class Program
     {
         Console.WriteLine($"Usage: {Environment.ProcessPath ?? "porth"} <SUBCOMMAND> [ARGS]");
         Console.WriteLine("  SUBCOMMANDS:");
-        Console.WriteLine("    sim <file>     Simulate the program");
-        Console.WriteLine("    com <file>     Compile the program");
-        Console.WriteLine("    help           Print this message");
+        Console.WriteLine("    sim <file>       Simulate the program");
+        Console.WriteLine("    com [-r] <file>  Compile the program");
+        Console.WriteLine("    help             Print this message");
     }
 
     private static void CallCommand(string[] args)
@@ -22,7 +22,9 @@ internal static class Program
             Arguments = string.Join(' ', args[1..]),
             WindowStyle = ProcessWindowStyle.Hidden,
             CreateNoWindow = true,
-            UseShellExecute = true
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
         };
 
         var process = Process.Start(startInfo);
@@ -32,6 +34,8 @@ internal static class Program
         }
 
         process.WaitForExit();
+        Console.Write(process.StandardOutput.ReadToEnd());
+        Console.Write(process.StandardError.ReadToEnd());
         if (process.ExitCode != 0)
         {
             Environment.Exit(process.ExitCode);
@@ -73,14 +77,28 @@ internal static class Program
             break;
             case "com":
             {
-                if (!argsIter.MoveNext())
+                var run = false;
+                string? programPath = null;
+                while (programPath == null && argsIter.MoveNext())
+                {
+                    var flag = (string)(argsIter.Current ?? throw new InvalidOperationException());
+                    switch (flag)
+                    {
+                        case "-r":
+                            run = true;
+                            break;
+                        default:
+                            programPath = flag;
+                            break;
+                    }
+                }
+                if (programPath == null)
                 {
                     Usage();
                     Console.WriteLine("[ERROR] no input file is provided for the compilation");
                     Environment.Exit(1);
                 }
 
-                var programPath = (string)(argsIter.Current ?? throw new InvalidOperationException());
                 var program = LoadProgramFromFile(programPath);
                 var outputPath = Path.ChangeExtension(programPath, ".rs") ?? throw new InvalidOperationException();
                 var outDir = Path.GetDirectoryName(programPath) ?? throw new InvalidOperationException();
@@ -88,6 +106,10 @@ internal static class Program
                 Compiler.Compile(program.ToList(), outputPath);
                 CallCommand(new[] { "rustfmt", outputPath });
                 CallCommand(new[] { "rustc", "-C", "opt-level=2", outputPath, "--out-dir", outDir });
+                if (run)
+                {
+                    CallCommand(new[] { Path.ChangeExtension(Path.GetFullPath(outputPath), ".exe") });
+                }
                 break;
             }
             case "help":
