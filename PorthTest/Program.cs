df85c56266e62a091d7996172336a02e6ab1fe04
Kyle Coffey <me@phytolizer.dev>
2022-01-28 00:53:59 -0600

    Allow specifying the test folder.

--- PorthTest/Program.cs
+++ PorthTest/Program.cs
@@ -1,104 +1,126 @@
-﻿using PorthTest;
+﻿namespace PorthTest;
 
-using var argsIter = ((IEnumerable<string>)args).GetEnumerator();
-
-if (!argsIter.MoveNext())
+internal static class Program
 {
-    Test();
-}
-else
-{
-    var subcommand = argsIter.Current;
-    switch (subcommand)
+    public static void Main(string[] args)
     {
-        case "record":
-            Record();
-            break;
-        case "test":
-            Test();
-            break;
-        case "help":
-            Usage();
-            break;
-        default:
-            Usage();
-            Console.WriteLine($"[ERROR] Unknown subcommand: {subcommand}.");
-            Environment.ExitCode = 1;
-            break;
-    }
-}
 
-void Usage()
-{
-    Console.WriteLine($"Usage: {Environment.ProcessPath ?? "PorthTest"} [SUBCOMMAND]");
-    Console.WriteLine("  SUBCOMMANDS:");
-    Console.WriteLine("    test    Run the tests. (Default operation when no subcommand is provided.)");
-    Console.WriteLine("    record  Record the expected output of the tests.");
-    Console.WriteLine("    help    Print this message.");
-}
+        using var argsIter = ((IEnumerable<string>)args).GetEnumerator();
 
-void Record()
-{
-    var tests = Path.Join(Environment.CurrentDirectory, "Tests");
-    foreach (var test in Directory.GetFiles(tests, "*.porth", SearchOption.AllDirectories))
-    {
-        var simOutput = RecordSimOutput(test);
-        var txtPath = Path.ChangeExtension(test, ".txt");
-        Console.WriteLine($"[INFO] Saving output to {txtPath}");
-        using var txtFile = File.CreateText(txtPath);
-        txtFile.Write(simOutput);
-    }
-}
+        var folder = Path.Join(Environment.CurrentDirectory, "Tests");
+        var subcommand = "test";
+        var subcommandFound = false;
 
-void Test()
-{
-    var tests = Path.Join(Environment.CurrentDirectory, "Tests");
-    foreach (var test in Directory.GetFiles(tests, "*.porth", SearchOption.AllDirectories))
-    {
-        Console.WriteLine($"[INFO] Testing {test}");
-        var txtPath = Path.ChangeExtension(test, ".txt");
-        var txtFile = File.OpenText(txtPath);
-        var expectedOutput = txtFile.ReadToEnd().ReplaceLineEndings("\n");
-        txtFile.Close();
-        var simOutput = RecordSimOutput(test);
-        if (simOutput != expectedOutput)
+        while (!subcommandFound && argsIter.MoveNext())
         {
-            Console.WriteLine("[ERROR] Unexpected simulation output");
-            Console.WriteLine("  Expected:");
-            Console.WriteLine(IndentMultiLineString(expectedOutput, "    "));
-            Console.WriteLine("  Actual:");
-            Console.WriteLine(IndentMultiLineString(simOutput, "    "));
-            throw new TestFailureException();
+            switch (argsIter.Current)
+            {
+                case "-f":
+                    if (!argsIter.MoveNext())
+                    {
+                        throw new UsageException("No folder is provided for option '-f'.");
+                    }
+
+                    folder = Path.GetFullPath(argsIter.Current);
+                    break;
+                default:
+                    subcommand = argsIter.Current;
+                    subcommandFound = true;
+                    break;
+            }
         }
-        var comOutput = RecordComOutput(test);
-        if (comOutput != expectedOutput)
+
+        switch (subcommand)
         {
-            Console.WriteLine("[ERROR] Unexpected simulation output");
-            Console.WriteLine("  Expected:");
-            Console.WriteLine(IndentMultiLineString(expectedOutput, "    "));
-            Console.WriteLine("  Actual:");
-            Console.WriteLine(IndentMultiLineString(comOutput, "    "));
-            throw new TestFailureException();
+            case "record":
+                Record(folder);
+                break;
+            case "test":
+                Test(folder);
+                break;
+            case "help":
+                Usage();
+                break;
+            default:
+                Usage();
+                Console.WriteLine($"[ERROR] Unknown subcommand: {subcommand}.");
+                Environment.ExitCode = 1;
+                break;
         }
-        Console.WriteLine($"[INFO] {test} OK");
     }
-}
 
-string RecordSimOutput(string s)
-{
-    var writer = new StringWriter();
-    PorthCs.Program.DoMain(new[] { "sim", s }, writer);
-    return writer.ToString().ReplaceLineEndings("\n");
-}
+    private static void Usage()
+    {
+        Console.WriteLine($"Usage: {Environment.ProcessPath ?? "PorthTest"} [SUBCOMMAND]");
+        Console.WriteLine("  SUBCOMMANDS:");
+        Console.WriteLine("    test    Run the tests. (Default operation when no subcommand is provided.)");
+        Console.WriteLine("    record  Record the expected output of the tests.");
+        Console.WriteLine("    help    Print this message.");
+    }
 
-string RecordComOutput(string s)
-{
-    var writer = new StringWriter();
-    PorthCs.Program.DoMain(new[] { "-q", "com", "-r", s }, writer);
-    return writer.ToString();
-}
+    private static void Record(string folder)
+    {
+        foreach (var test in Directory.GetFiles(folder, "*.porth", SearchOption.AllDirectories))
+        {
+            var simOutput = RecordSimOutput(test);
+            var txtPath = Path.ChangeExtension(test, ".txt");
+            Console.WriteLine($"[INFO] Saving output to {txtPath}");
+            using var txtFile = File.CreateText(txtPath);
+            txtFile.Write(simOutput);
+        }
+    }
 
-string IndentMultiLineString(string str, string indent)
-{
-    return string.Join("\n", str.Split("\n").Select(line => indent + line));
+    private static void Test(string folder)
+    {
+        foreach (var test in Directory.GetFiles(folder, "*.porth", SearchOption.AllDirectories))
+        {
+            Console.WriteLine($"[INFO] Testing {test}");
+            var txtPath = Path.ChangeExtension(test, ".txt");
+            var txtFile = File.OpenText(txtPath);
+            var expectedOutput = txtFile.ReadToEnd().ReplaceLineEndings("\n");
+            txtFile.Close();
+            var simOutput = RecordSimOutput(test);
+            if (simOutput != expectedOutput)
+            {
+                Console.WriteLine("[ERROR] Unexpected simulation output");
+                Console.WriteLine("  Expected:");
+                Console.WriteLine(IndentMultiLineString(expectedOutput, "    "));
+                Console.WriteLine("  Actual:");
+                Console.WriteLine(IndentMultiLineString(simOutput, "    "));
+                throw new TestFailureException();
+            }
+
+            var comOutput = RecordComOutput(test);
+            if (comOutput != expectedOutput)
+            {
+                Console.WriteLine("[ERROR] Unexpected simulation output");
+                Console.WriteLine("  Expected:");
+                Console.WriteLine(IndentMultiLineString(expectedOutput, "    "));
+                Console.WriteLine("  Actual:");
+                Console.WriteLine(IndentMultiLineString(comOutput, "    "));
+                throw new TestFailureException();
+            }
+
+            Console.WriteLine($"[INFO] {test} OK");
+        }
+    }
+
+    private static string RecordSimOutput(string s)
+    {
+        var writer = new StringWriter();
+        PorthCs.Program.DoMain(new[] { "sim", s }, writer);
+        return writer.ToString().ReplaceLineEndings("\n");
+    }
+
+    private static string RecordComOutput(string s)
+    {
+        var writer = new StringWriter();
+        PorthCs.Program.DoMain(new[] { "-q", "com", "-r", s }, writer);
+        return writer.ToString();
+    }
+
+    private static string IndentMultiLineString(string str, string indent)
+    {
+        return string.Join("\n", str.Split("\n").Select(line => indent + line));
+    }
 }
---
+++ PorthTest/UsageException.cs
@@ -0,0 +1,8 @@
+﻿namespace PorthTest;
+
+public class UsageException : Exception
+{
+    public UsageException(string message) : base(message)
+    {
+    }
+}
